3f9d5d9344994d3e472b116a104cbf636a595be8
BryanAislanZinn <bryanrock.zinn@gmail.com>
2022-08-02 13:51:07 -0300

    Adicionando tela de detalhes

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product | undefined;
+
+  constructor( private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
+      
+      // pega o id do produto da rota  
+      const routeParams = this.route.snapshot.paramMap;
+      const productIdFromRoute = Number(routeParams.get('productId'));
+  
+      // encontra o produto que corresponde ao id passado na rota
+      this.product = products.find(
+          product => product.id === productIdFromRoute
+      );  
   }
 
 }
